b7f345e9b478bd7be8810d8658a39b88185cd5f3
bfindley <bfindley28@stsaviour.org>
2026-03-11 11:44:51 -0400

    stardew alley

--- garden.py
+++ garden.py
@@ -3,4 +3,55 @@
 if __name__ == '__main__':
 
     # print without a line break using this function
-    print('🌷', end='')
+    # print('🌷', end='')
+
+    # i'm using these emojis - 💕 🥀
+
+    print('')
+    print('solid')
+    for i in range(11):
+        for j in range(11):
+            print('💕', end='')
+        print('')
+
+    print('')
+    print('horizontal')
+    for i in range(11):
+        for j in range(11):
+            if i % 2 == 0:
+                print('💕', end='')
+            else:
+                print('🥀', end='')
+        print('')
+
+    print('')
+    print('vertical')
+    for i in range(11):
+        for j in range(11):
+            if j % 2 == 0:
+                print('💕', end='')
+            else:
+                print('🥀', end='')
+        print('')
+
+    print('')
+    print('diagonal')
+    for i in range(11):
+        for j in range(11):
+            if i % 2 == 0 and j % 2 == 0:
+                print('💕', end='')
+            elif i % 2 == 1 and j % 2 == 1:
+                print('💕', end='')
+            else:
+                print('🥀', end='')
+        print('')
+
+    print('')
+    print('plaid')
+    for i in range(11):
+        for j in range(11):
+            if j % 2 == 0 and i % 2 == 0:
+                print('💕', end='')
+            else:
+                print('🥀', end='')
+        print('')
